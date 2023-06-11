ba2f43a63e129fbe90d73c4af05e2c3e5ae3dc90
ALEA2022 <133906012+ALEA2022@users.noreply.github.com>
2023-06-11 18:29:26 +0200

    Update connex.php

--- connex.php
+++ connex.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="crea.css">
+<link rel="stylesheet" href="crea.css"> <!-- Lien vers la feuille de style crea.css -->
 <title>Connexion</title>
 </head>
 <body>
 <div class="container">
-        <img src="logoJeunes.png" alt="Image" class="logo">
+        <img src="logoJeunes.png" alt="Image" class="logo"> <!-- Logo -->
 <form method="post" action="">
-    <h4><i>CONNEXION</i></h4>
+    <h4><i>CONNEXION</i></h4> <!-- Titre de la section -->
     <br><br> <br>
-    <input type="email" name="email" placeholder="Email"><br>
-   <input type="password" name="password" placeholder="Mot de passe"><br>
+    <input type="email" name="email" placeholder="Email"><br> <!-- Champ de saisie de l'email -->
+   <input type="password" name="password" placeholder="Mot de passe"><br> <!-- Champ de saisie du mot de passe -->
    <br>
-    <input type="submit" name="submit" value="Se connecter"><br>
-    <a href="mdp.php" class="btn" id="signin">Mot de passe oublié?</a>
+    <input type="submit" name="submit" value="Se connecter"><br> <!-- Bouton de soumission du formulaire -->
+    <a href="mdp.php" class="btn" id="signin">Mot de passe oublié?</a> <!-- Lien vers la page mdp.php avec un style personnalisé -->
 </form>
 </div>
+
+        
+        
 <?php
 session_start();
 
@@ -28,14 +31,14 @@ if (isset($_POST['submit'])) {
 
     while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
         if ($data[0] == $email && $data[3] == $password) {
-            echo "<p style='color: white; margin-top:40%; margin-left:-19%;'>Connecté avec succès!</p>";
+            echo "<p style='color: white; margin-top:40%; margin-left:-19%;'>Connecté avec succès!</p>"; // Message de succès de connexion
             $_SESSION["user_email"] = $email;
-            header("Location: accueilJeunes.php");
+            header("Location: accueilJeunes.php"); // Redirection vers la page accueilJeunes.php
             fclose($file);
             return;
         }
     }
-    echo "<p style='color: white; margin-top:40%; margin-left:-19%;'>Identifiant ou mot de passe incorrect</p>";
+    echo "<p style='color: white; margin-top:40%; margin-left:-19%;'>Identifiant ou mot de passe incorrect</p>"; // Message d'erreur d'identification
     fclose($file);
 }
 ?>
