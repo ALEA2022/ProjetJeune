5d7a12a225d9fb8a59b174079c11a680a2fd4a56
ALEA2022 <133906012+ALEA2022@users.noreply.github.com>
2023-06-11 18:24:36 +0200

    Update administrateur.php

--- administrateur.php
+++ administrateur.php
@@ -3,9 +3,9 @@
   <head>
     <title>Mon Site</title>
     <meta charset="UTF-8">
-    <meta name="description" content="Description de mon site.">
-    <meta name="keywords" content="Mots-clés, pour, mon, site.">
-    <link rel="stylesheet" href="jeunes.css">
+    <meta name="description" content="Description de mon site."> <!-- Balise de description du site -->
+    <meta name="keywords" content="Mots-clés, pour, mon, site."> <!-- Balise de mots-clés pour le référencement -->
+    <link rel="stylesheet" href="jeunes.css"> <!-- Lien vers la feuille de style jeunes.css -->
   </head>
   <p class="footer-text"><span style="font-size: 80px; color: rgb(255, 152, 9)"; > ADMINISTRATEUR</p></span>
   <body>
@@ -13,8 +13,7 @@
       
       <header class="top-bar">
         <div class="element">
-
-          <img src="logoJeunes.png" alt="Image">
+          <img src="logoJeunes.png" alt="Image"> <!-- Logo -->
         </div>
     
       </header>
@@ -22,19 +21,18 @@
 
         <ul class="menu">
           <li class="menuj">
-            <a href="jeunes.php" class="jeunes">JEUNES</a>
+            <a href="jeunes.php" class="jeunes">JEUNES</a> <!-- Lien vers la page jeunes.php -->
           </li>
           <li class="menur">
-            <a href="référent.html" class="referent">REFERENT</a>
+            <a href="référent.html" class="referent">REFERENT</a> <!-- Lien vers la page référent.html -->
           </li>
           <li class="menuc">
-            <a href="consultant.html" class="consultant">CONSULTANT</a>
+            <a href="consultant.html" class="consultant">CONSULTANT</a> <!-- Lien vers la page consultant.html -->
           </li>
           <li class="menup">
-            <a href="partenaires.html" class="partenaires">PARTENAIRES</a>
+            <a href="partenaires.html" class="partenaires">PARTENAIRES</a> <!-- Lien vers la page partenaires.html -->
           </li>
           <li class="menua">
-            <a href="administrateur.html" class="administrateur"><span style="background-color:  rgb(255, 152, 9); color: white";>ADMINISTRATEUR</a>
+            <a href="administrateur.html" class="administrateur"><span style="background-color:  rgb(255, 152, 9); color: white";>ADMINISTRATEUR</a> <!-- Lien vers la page administrateur.html avec un style personnalisé -->
           </li>
         </ul>
-        
